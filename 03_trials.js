fc6ac95221b6da4aa02cf9204fe3b541a4883636
marisawodrich <mwodrich@uni-osnabrueck.de>
2019-05-15 12:37:55 +0200

    initial commit of departure-point with keyPress tasks

---
+++ 03_trials.js
@@ -0,0 +1,25 @@
+// In this file you can specify the trial data for your experiment
+
+
+const trial_info = {
+    forced_choice: [
+        {
+            question: "What's on the bread?",
+            picture: "images/question_mark_02.png",
+            key1: 'q',
+            key2: 'p',
+            q: 'ham',
+            p: 'jam',
+            expected: 'jam'
+        },
+        {
+            question: "What's the weather like?",
+            picture: "images/weather.jpg",
+            key1: 'q',
+            key2: 'p',
+            q: 'shiny',
+            p: 'rainbow',
+            expected: 'rainbow'
+        }
+    ]
+};
